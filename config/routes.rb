8faf2624ba5dca21e13d24adde74bb8a175d3fc6
Leonardo Brito <f.leobrito@gmail.com>
2023-10-06 21:59:52 -0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "sidekiq/web"
+
+Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq"
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get "/public_method", to: "hello_world#public_method"
+  get "/private_method", to: "hello_world#private_method"
+  get "/search", to: "hello_world#search"
+end
