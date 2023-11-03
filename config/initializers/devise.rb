5a9f87edb3a79adc00b0c3d274b649ef4e17f861
leonardobrito <f.leobrito@gmail.com>
2023-11-03 20:43:27 -0300

    Change envs to default equal nil

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,7 +262,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :strava, ENV.fetch("STRAVA_CLIENT_ID").to_i, ENV.fetch("STRAVA_CLIENT_SECRET"),
+  config.omniauth :strava, ENV.fetch("STRAVA_CLIENT_ID", nil).to_i, ENV.fetch("STRAVA_CLIENT_SECRET", nil),
     scope: ENV.fetch("STRAVA_SCOPE"),
     token_params: {
       client_id: ENV.fetch("STRAVA_CLIENT_ID", nil),
