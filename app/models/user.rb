efddd74d6eb810d35dc604b74d24d855dbbf4679
leonardobrito <f.leobrito@gmail.com>
2023-11-03 21:52:00 -0300

    Update user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  ALLOWED_PROVIDERS_HASH = { strava: "strava" }.freeze
   update_index("users") { self }
 
   # Include default devise modules. Others available are:
@@ -15,10 +16,15 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    find_or_create_by(oauth_provider: auth.provider, oauth_uid: auth.uid) do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
+    oauth_provider = auth.provider
+
+    case oauth_provider
+    when ALLOWED_PROVIDERS_HASH[:strava]
+      find_or_create_by(oauth_provider: auth.provider, oauth_uid: auth.uid) do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.name = auth.info.name
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,35 @@ RSpec.describe User do
   subject { build(:user) }
 
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+
+  describe "#from_omniauth" do
+    subject(:user) { described_class.first }
+
+    let(:uid) { "1234" }
+    let(:info) { { name: "Athlete", email: nil } }
+    let(:provider) { "strava" }
+    let(:oauth_provider_data) do
+      OmniAuth::AuthHash.new({ provider:, uid:, info: })
+    end
+
+    before { described_class.from_omniauth(oauth_provider_data) }
+
+    context "when oauth_provider is strava" do
+      it "match oauth_uid" do
+        expect(user.oauth_uid).to eq(uid)
+      end
+
+      it "match oauth_provider" do
+        expect(user.oauth_provider).to eq(provider)
+      end
+    end
+
+    context "when oauth_provider is not supported" do
+      let(:provider) { "facebook" }
+
+      it "not persist the user" do
+        expect(user).to be_nil
+      end
+    end
+  end
 end
